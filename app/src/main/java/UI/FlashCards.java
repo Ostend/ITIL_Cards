ce1a3d8bad9b9cddeea4ae14789c5f9ae67142ab
ostend <smennear.nccu@gmail.com>
2022-06-09 03:22:19 -0400

    Added Shuffle Terms functionality + wrong answer and frequency tracking

--- app/src/main/java/UI/FlashCards.java
+++ app/src/main/java/UI/FlashCards.java
@@ -15,9 +15,12 @@ import com.example.itil_cards.R;
 
 import java.nio.channels.ReadPendingException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Random;
 import java.util.logging.Logger;
 
 import DataBase.Repository;
@@ -25,19 +28,22 @@ import Entities.Card;
 
 public class FlashCards extends AppCompatActivity {
     //todo add ability to flip the cards so user sees answer and has to guess term
-    //todo create side list of all 'wrong' answers
+    //todo create hashmap of all 'wrong' answers and frequency of wrong
+    //todo shuffle terms
+
     //if term, cardStatus = 0, if answer cardStatus = 1
     private int correctNum = 1;
     private int pos = 0;
     private int cardStatus = 0;
     private List<Card> allCards = new ArrayList<>();
-    private Queue<Card> qCards = new PriorityQueue<>();
+    private HashMap<Card, Integer> wrongAnswers = new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_cards);
         Repository repo = new Repository(getApplication());
         allCards = repo.getmAllCards();
+        Collections.shuffle(allCards, new Random());
         Log.d("allCards", String.valueOf(allCards.size()));
 
         //Set the flash card with the term:
@@ -49,10 +55,17 @@ public class FlashCards extends AppCompatActivity {
     }
 
     public void wrongButton(View view) {
+        //todo Add Hashmap to track frequency of specific wrong answers
+        //todo if answer wrong more than twice, add duplicate card into flashcards
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 allCards.add(allCards.remove(pos));
+                if( wrongAnswers.containsKey(allCards.get(allCards.size() - 1)) ){
+                    wrongAnswers.replace(allCards.get(allCards.size() - 1), (wrongAnswers.get(allCards.get(allCards.size() - 1)) + 1));
+                }else {
+                    wrongAnswers.put(allCards.get(allCards.size() - 1), 1);
+                }
                 ((TextView) findViewById(R.id.cardText)).setText(allCards.get(pos).getTerm());
                 cardStatus = 0;
             }
@@ -63,6 +76,7 @@ public class FlashCards extends AppCompatActivity {
 
     public void repeatButton(View view) {
         //pos = pos + 1;
+
         allCards.add(allCards.remove(pos));
         ((TextView) findViewById(R.id.cardText)).setText(allCards.get(pos).getTerm());
         cardStatus = 0;
@@ -79,6 +93,8 @@ public class FlashCards extends AppCompatActivity {
             check.setEnabled(false);
             TextView cardV = findViewById(R.id.cardText);
             cardV.setOnClickListener(null);
+            ((ImageButton) findViewById(R.id.repeatButton)).setEnabled(false);
+            ((ImageButton) findViewById(R.id.wrongButton)).setEnabled(false);
             return;
         }else{((TextView) findViewById(R.id.cardText)).setText(allCards.get(pos).getTerm());
             ((TextView) findViewById(R.id.numberText)).setText( correctNum + "/" + allCards.size());
@@ -93,11 +109,10 @@ public class FlashCards extends AppCompatActivity {
         if (cardStatus == 0) {
             cardStatus = 1;
             ((TextView) findViewById(R.id.cardText)).setText(allCards.get(pos).getAnswer());
-            return;
         }else{
             cardStatus = 0;
             ((TextView) findViewById(R.id.cardText)).setText(allCards.get(pos).getTerm());
-            return;
         }
+        return;
     }
 }
